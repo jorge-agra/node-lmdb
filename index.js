d16a3ab0878ad6383e8d7d77bf0632a3cbdffbfb
Jorge Agra <jorge.agra@live.com>
2019-06-19 19:14:04 +0100

    12

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 'use strict';
 
-module.exports = require('bindings')('node-lmdb.node');
+//module.exports = require('bindings')('node-lmdb.node');
+module.exports = require('node-gyp-build')(__dirname)
